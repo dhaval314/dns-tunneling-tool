ff808ad3bb853d2f3cbeb1ada7eb37a90f322bc2
dhaval314 <bondedhaval@gmail.com>
2025-07-30 19:40:11 +0530

    Added bi-directional functionality and handles chunks arriving out of order, NOTE: ip address of client is hardcoded for now

--- client.py
+++ client.py
@@ -3,11 +3,14 @@ import subprocess
 import optparse
 from encoder import encode_data
 import socket
+import math
 
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-m", "--message", dest ="message")
-    parser.add_option("-s", "--session", dest="session_id")
+    parser.add_option("-m", "--message", dest = "message")
+    parser.add_option("-s", "--session", dest = "session_id")
+    parser.add_option("-i", "--ip", dest = "ip", help="IP address of the DNS server")
+    parser.add_option("-p", "--port", dest = "port")
     (options, arguements) = parser.parse_args()
 
     return options
@@ -19,29 +22,44 @@ def create_request(domain):
 
 options = get_arguments()
 encoded_data = encode_data(options.message)
-encoded_data += "eom"
+
 session_id = options.session_id
 
 
 
-dns_server_ip = "127.0.0.1"
-dns_server_port = 53
+dns_server_ip = options.ip
+dns_server_port = int(options.port)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("127.0.0.1", int(options.port)))
 
 n = len(encoded_data)
 chunk_size = 63
+total_chunks = math.ceil((n/63))
 chunk = 0
 for i in range(0, n, chunk_size):
     chunk_data = encoded_data[i:i+chunk_size]
     chunk_id = str(chunk).zfill(2)
-    domain = f"{chunk_id}.{chunk_data}.{session_id}.tunnel.local"
+    domain = f"{chunk_id}.{total_chunks}.{chunk_data}.{session_id}.tunnel.local"
     query = create_request(domain)
     sock.sendto(query.pack(), (dns_server_ip, dns_server_port))
     print(f"Sent {chunk_id} {chunk_data}")
     chunk += 1
 
+while 1:
+    data, addr = sock.recvfrom(514)
+    recieved_response = DNSRecord.parse(data)
+    if recieved_response:
+        print("DNS server recieved the message successfully")
+        break
 
+# TODO / Improvements
+
+# [+] Handle chunk reordering with sorting
+# [ ] Add support for multiple simultaneous sessions
+# [ ] Send responses back to the client (bi-directional)
+# [ ] Encrypt the data before encoding
+# [ ] Add file transfer support
 
 
 
--- server.py
+++ server.py
@@ -1,32 +1,50 @@
 import socket
-from dnslib import DNSRecord
+from dnslib import DNSRecord, DNSHeader, DNSQuestion, RR, A
 import re
 from encoder import decode_data
-import time
+from collections import defaultdict
 port = 53
-ip = "127.0.0.1"
+ip = "127.0.0.2"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind((ip, port))
-message = ""
-
+message = defaultdict()
+final_encoded_message = ""
+chunks = 0
 while 1:
     data, addr = sock.recvfrom(512)
+
     response = DNSRecord.parse(data)
-    pattern = r"\d+\.[A-Za-z\d]+\.[A-Za-z\d]+\.[A-Za-z\d]+\.[A-Za-z\d]+"
+    pattern = r"\d+\.\d+\.[A-Za-z\d]+\.[A-Za-z\d]+\.[A-Za-z\d]+\.[A-Za-z\d]+" # pattern for the dns query
     matched = re.search(pattern, str(response))
     if matched:
-        encoded_message = matched.group(0).split(".")
-        message += encoded_message[1]  
-    # print(message[-3:])
-    if message[-3:] == "eom":
-        decoded_message = decode_data(message[:-3])
-        print(decoded_message)
-        message = ""
-   
-    
-    
-    
+        encoded_message = matched.group(0).split(".") 
+        message[int(encoded_message[0])] = encoded_message[2]
+        chunks += 1
+        
 
+    # if we recieve the last chunk
+    if int(matched.group(0).split(".")[1]) == chunks:
 
+        arranged_msg = dict(sorted(message.items())) # sorting if the chunks are recieved out of order
+
+        for encoded_chunk in arranged_msg.values():
+            final_encoded_message += encoded_chunk
+        
+        final_decoded_message = decode_data(final_encoded_message)
+        print(final_decoded_message)
+
+        # resetting for data that might be sent later
+        chunks = 0
+        final_encoded_message = ""
+
+        # responding when the message is fully recieved
+        response = DNSRecord(DNSHeader(qr=1,aa=1,ra=1),
+                  q=DNSQuestion(""),
+                  a=RR("",rdata=A("123.12.12.12")))
+        
+        sock.sendto(response.pack(), (addr[0], addr[1]))
+        response = None
+        
+    
